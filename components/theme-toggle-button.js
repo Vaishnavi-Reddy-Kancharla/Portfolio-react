1e29ac65c7937916859a4af70a8c654bf1986b69
“vaishnavi” <vaishnavi.kancharla@research.iiit.ac.in>
2023-06-07 22:10:47 +0530

    changes to package.json

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -9,7 +9,7 @@ const ThemeToggleButton = () => {
         <AnimatePresence exitBeforeEnter initial={false}>
             <motion.div
                 style={{ display: 'inline-block' }}
-                key={ useColorModeValue('dark', 'light') }
+                key={ useColorModeValue('light', 'dark') }
                 initial={{ y: -20, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: 20, opacity: 0 }}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-    reactStrictMode: true
-  }
+const nextConfig = {
+  distDir: "build",
+};
+
+module.exports = nextConfig;
